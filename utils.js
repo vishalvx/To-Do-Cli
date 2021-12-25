e75ec251ca01f451973acf1bbbb317244cf62a16
Vishal <vishalp000111@gmail.com>
2021-12-25 09:30:53 +0530

    completed solution

--- utils.js
+++ utils.js
@@ -4,7 +4,6 @@ const readData = async () => {
   let data;
   try {
     data = await readFile(new URL("task.txt", import.meta.url), "utf-8");
-    // console.log(typeof(data));
   } catch (error) {
     await writeData([]);
     data = "[]";
@@ -34,14 +33,7 @@ export const help = async () => {
  */
 export const add = async (priority, task) => {
   const preTask = await readData();
-  //   console.log(preTask);
-
-  //do not add task which are already exists with same priority and task (for 5 testcast)
-  if (
-    !preTask.find((value) => value.priority == priority && value.task == task)
-  )
-    await writeData([...preTask, { priority, task, isDone: false }]);
-
+  await writeData([...preTask, { priority, task, isDone: false }]);
   console.log(`Added task: "${task}" with priority ${priority}`);
 };
 
@@ -49,16 +41,20 @@ export const add = async (priority, task) => {
  * Ls Command Utils
  */
 export const listTask = async () => {
+  //getting Tasks
   const tasks = await readData();
-  if (tasks.length === 0) {
+  const pendingTasks = tasks.filter((task) => !task.isDone);
+  const completedTasks = tasks.filter((task) => task.isDone);
+
+  if (pendingTasks.length === 0) {
     console.log("There are no pending tasks!");
   } else {
-    tasks.sort((a, b) => {
+    pendingTasks.sort((a, b) => {
       return a.priority - b.priority;
     });
     //for add sorted task in json For Done Command
-    await writeData(tasks);
-    tasks.map((value, index) => {
+    await writeData([...pendingTasks, ...completedTasks]);
+    pendingTasks.map((value, index) => {
       console.log(`${index + 1}. ${value.task} [${value.priority}]`);
     });
   }
@@ -70,10 +66,17 @@ export const listTask = async () => {
 export const done = async (index) => {
   //fetching json data
   const tasks = await readData();
+  const pendingTasks = tasks.filter((task) => !task.isDone);
+  const completedTasks = tasks.filter((task) => task.isDone);
+
+  pendingTasks.sort((a, b) => {
+    return a.priority - b.priority;
+  });
+
   // marking as Done
-  tasks[index - 1].isDone = true;
+  pendingTasks[index - 1].isDone = true;
   //write in the file
-  await writeData(tasks);
+  await writeData([...pendingTasks, ...completedTasks]);
   //logged on terminal
   console.log("Marked item as done.");
 };
@@ -115,21 +118,24 @@ export const deleteTask = async (indexs) => {
  */
 export const report = async () => {
   const tasks = await readData();
+  const completedTasks = tasks
+    .filter((task) => task.isDone)
+    .sort((a, b) => {
+      return a.priority - b.priority;
+    });
+  const pendingTasks = tasks
+    .filter((task) => !task.isDone)
+    .sort((a, b) => {
+      return a.priority - b.priority;
+    });
 
-  // copyTasks = tasks;
+  console.log(`Pending : ${pendingTasks.length}`);
+  pendingTasks.map((value, index) => {
+    console.log(`${index + 1}. ${value.task} [${value.priority}]`);
+  });
 
-  const completedList = tasks.filter((task) => task.isDone);
-  const incompletedList = tasks.filter((task) => !task.isDone);
-
-  console.log(
-    `Pending : ${incompletedList.length}\n${incompletedList
-      .map((ic, idx) => {
-        return idx + 1 + ". " + ic.task + " [" + ic.priority + "]";
-      })
-      .join("\n")}\nCompleted : ${completedList.length}\n${completedList
-      .map((ct, index) => {
-        return `${index + 1}. ${ct.task}`;
-      })
-      .join("\n")}`
-  );
+  console.log(`\nCompleted : ${completedTasks.length}`);
+  completedTasks.map((value, index) => {
+    console.log(`${index + 1}. ${value.task}`);
+  });
 };
